85b4eaf91d109d1f354d9b7b81d0d6e8d7002837
"Abiola Fasanya" <"hezekiahfasanya@gmail.com">
2022-03-09 21:32:43 +0100

    testing

---
+++ contract/HelloWorld.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+    uint public messageCount = 0;
+    string public message;
+
+    constructor()  {
+        setMsg("Hello World");
+    }
+
+    function getMsg() public view returns(string memory) {
+        return message;
+    }
+
+    function setMsg(string memory _message) public {
+        messageCount++;
+        message = _message;
+    }
+
+}
